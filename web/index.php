e8e1ebf6440fcd8126bb1d7601ae6b595cb2dfaf
yousef-saddik77 <yousefsaddik77@gmail.com>
2025-05-05 21:03:58 +0300

    fixing font file path

--- web/index.php
+++ web/index.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/static/css/master.css" />
-    <link rel="stylesheet" href="/static/css/nav.css" />
+    <link rel="stylesheet" href="./static/css/master.css" />
+    <link rel="stylesheet" href="./static/css/nav.css" />
     <script defer src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script defer src="master.js"></script>
+    <script defer src="./static/js/main.js"></script>
     <title>hotpot</title>
 
 </head>
