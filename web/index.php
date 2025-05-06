afe1039fcd83d00a49aa126a28a98a855a2ae426
yousef-saddik77 <yousefsaddik77@gmail.com>
2025-05-06 18:24:50 +0300

    adding the base for cart and dashboard

--- web/index.php
+++ web/index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="./static/css/master.css" />
     <link rel="stylesheet" href="./static/css/nav.css" />
+    <link rel="stylesheet" href="./static/css/card.css" />
+    <link rel="stylesheet" href="./static/css/footer.css" />
     <script defer src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script defer src="./static/js/main.js"></script>
     <script defer src="./static/js/debug.js"></script>
@@ -18,7 +20,7 @@
 <body>
     <?php require_once("./MVC/views/common/nav.html"); ?>
 
-    <?php require_once("./MVC/views/cards.html"); ?>
+    <?php require_once("./MVC/views/dashboard.html"); ?>
 
     <?php require_once("./MVC/views/common/footer.html"); ?>
 
